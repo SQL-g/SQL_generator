ac9da78ab959ea78789aa4424214227cbc0a8ae4
Sergiy Alariki <sergiy.alariki.21@gmail.com>
2020-01-25 15:43:20 -0500

    Add table rows

--- client/components/Row.jsx
+++ client/components/Row.jsx
@@ -5,9 +5,25 @@ export class Row extends Component {
         return (
             <tr>
                 <td><input type="text"/></td>
-                <td></td>
-                <td><input type="text"/></td>
-                <td><input type="text"/></td>
+                <td>
+                    <select name="" id="">
+                        <option value="integer">integer</option>
+                        <option value="varchar">varchar</option>
+                        <option value="timestamp">timestamp</option>
+                    </select>
+                </td>
+                <td>
+                    <select name="" id="">
+                        <option value="yes">yes</option>
+                        <option value="yes">no</option>
+                    </select>
+                </td>
+                <td>
+                    <select name="" id="">
+                        <option value="yes">yes</option>
+                        <option value="yes">no</option>
+                    </select>
+                </td>
                 <td><input type="text"/></td>
             </tr>
         )
--- client/components/Table.jsx
+++ client/components/Table.jsx
@@ -2,20 +2,41 @@ import React, { Component } from 'react';
 import { Row } from './Row'
 
 export class Table extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            rows: 0,
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        const rows = this.state.rows + 1;
+        this.setState({ rows });
+    }
+
     render() {
+
+        const rows = Array.from({ length: this.state.rows }, (v, i) => <Row key={i}/>);
+
         return (
             <div>
                 <h1>Table</h1>
                 <table>
-                    <tr>
-                        <th>Name</th>
-                        <th>Type</th>
-                        <th>Is Required</th>
-                        <th>Is Unique</th>
-                        <th>Default</th>
-                    </tr>
-                    <Row />
+                    <tbody>
+                        <tr>
+                            <th>Name</th>
+                            <th>Type</th>
+                            <th>Is Required</th>
+                            <th>Is Unique</th>
+                            <th>Default</th>
+                        </tr>
+                        {rows}
+                    </tbody>
                 </table>
+                <button onClick={this.handleClick}>Add field</button>
+                <br/>
+                <button>Submit</button>
             </div>
         )
     }
