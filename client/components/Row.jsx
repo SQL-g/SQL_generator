073d6b90989ed17c033a734b7106e59100a9029c
Sergiy Alariki <sergiy.alariki.21@gmail.com>
2020-01-26 12:49:15 -0500

    resolving the checkbox conflict

--- client/components/Row.jsx
+++ client/components/Row.jsx
@@ -1,30 +1,39 @@
 import React, { Component } from 'react';
 
 export class Row extends Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this)
+    }
+    
+    handleChange(e) {
+        e.preventDefault();
+
+        this.props.handleChange(
+            this.props.idx,
+            e.target.name,
+            e.target.type === 'checkbox' ? e.target.checked : e.target.value,
+        );
+    }
+
+    componentDidUpdate() {
+        console.log('UPDATE', JSON.stringify(this.props))
+    }
+
     render() {
         return (
             <tr>
-                <td><input type="text"/></td>
+                <td><input type="text" name="name" value={this.props.name} onChange={this.handleChange} required /></td>
                 <td>
-                    <select name="" id="">
+                    <select name="type" value={this.props.type} onChange={this.handleChange}>
                         <option value="integer">integer</option>
                         <option value="varchar">varchar</option>
                         <option value="timestamp">timestamp</option>
                     </select>
                 </td>
-                <td>
-                    <select name="" id="">
-                        <option value="yes">yes</option>
-                        <option value="yes">no</option>
-                    </select>
-                </td>
-                <td>
-                    <select name="" id="">
-                        <option value="yes">yes</option>
-                        <option value="yes">no</option>
-                    </select>
-                </td>
-                <td><input type="text"/></td>
+                <td><input type="checkbox" name="isRequired" checked={this.props.isRequired} onChange={this.handleChange} /></td>
+                <td><input type="checkbox" name="isUnique" checked={this.props.isUnique} onChange={this.handleChange} /></td>
+                <td><input type="text" name="default" value={this.props.default} onChange={this.handleChange} /></td>
             </tr>
         )
     }
--- client/components/Table.jsx
+++ client/components/Table.jsx
@@ -5,49 +5,72 @@ export class Table extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            rows: 0,
             data: [],
+            bools: [],
         };
         this.handleClick = this.handleClick.bind(this);
+        this.handleChange=this.handleChange.bind(this);
+        this.handleBool=this.handleBool.bind(this);
     }
 
     handleClick() {
-        const rows = this.state.rows + 1;
-        const data = this.state.data.slice();
-        data.push({
-            name: '',
-            type: '',
-            isUnique: '',
-            isRequired: '',
-            default: '',
-        });
-        console.log('data array:', data);
-        this.setState({ rows, data });
-        console.log(this.state);
+        // this.setState({ data: [...this.state.data, {
+        //     name: '',
+        //     type: 'integer',
+        //     isUnique: false,
+        //     isRequired: true,
+        //     default: '',
+        // }]});
+        this.setState({ bools: [...this.state.bools, true]})
+    }
+
+    handleChange(index, property, value) {
+        console.log(index, property, value);
+        console.log(JSON.stringify(this.state.data.slice(0, index)));
+        this.setState({ data: [
+            ...this.state.data.slice(0, index),
+            { ...this.state.data[index], [property]: value },
+            ...this.state.data.slice(index + 1),
+        ]}, () => { console.log(JSON.stringify(this.state)); });
+    }
+    handleBool(index, val) {
+        this.setState({ bools: [
+            ...this.state.bools.slice(0, index),
+            val,
+            ...this.state.bools.slice(index + 1),
+        ]}, () => { console.log(JSON.stringify(this.state)); });
     }
 
     render() {
-
-        const rows = Array.from({ length: this.state.rows }, (v, i) => <Row key={i}/>);
-
         return (
             <div>
                 <h1>Table</h1>
-                <table>
-                    <tbody>
-                        <tr>
-                            <th>Name</th>
-                            <th>Type</th>
-                            <th>Is Required</th>
-                            <th>Is Unique</th>
-                            <th>Default</th>
-                        </tr>
-                        {rows}
-                    </tbody>
-                </table>
-                <button onClick={this.handleClick}>Add field</button>
-                <br/>
-                <button>Submit</button>
+                <form>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <th>Name</th>
+                                <th>Type</th>
+                                <th>Is Required</th>
+                                <th>Is Unique</th>
+                                <th>Default</th>
+                            </tr>
+                            {
+                                this.state.data.map(
+                                    (row, i) => <Row key={i} { ...row } idx={i} handleChange={this.handleChange} />
+                                )
+                            }
+                            {
+                                this.state.bools.map((val, i) => (
+                                    <tr><td><input type="checkbox" name="isRequired" checked={val} onChange={(e) => this.handleBool(i, e.target.checked)} /></td></tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                    <button onClick={this.handleClick} type='button'>Add field</button>
+                    <br/>
+                    <button type='submit'>Submit</button>
+                </form>
             </div>
         )
     }
