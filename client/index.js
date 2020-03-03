3c0b594e9c41eaa449e87a9b60a95aedd3f2cd70
Sergiy Alariki <sergiy.alariki.21@gmail.com>
2020-03-03 11:09:10 -0500

    chronoscope

--- client/index.js
+++ client/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import { App } from './App';
 import './stylesheets/styles.css';
+import cronoscope from 'react-chronoscope';
 
 
 render (
     <App/>,
     document.getElementById('root')
 )
+
+cronoscope(document.getElementById('root'));
